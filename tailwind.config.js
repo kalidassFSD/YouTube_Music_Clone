9e51fcbeac6f172d9ee014c762d79f389a8c5fcb
Kalidass G <149503695+kalidassFSD@users.noreply.github.com>
2024-10-24 20:09:43 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./build/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+          Roboto:['Roboto','Arial']
+      },
+      spacing:{
+          '56':'3.5rem',
+          '536':'33.5rem',
+          '225':'14rem',
+          '60':'3.75rem',
+          '4.6':'4.6rem',
+          '2.4': '2.4rem',
+          '74':'74px',
+          'x':'10px',
+          '399'  :'399px',
+          '224':'224px'
+          
+      }
+    },
   },
   plugins: [],
 }
